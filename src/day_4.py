aab307485e64ca7d5559359cf3267fe4d71f4753
Peter Lukacs <lukacs.peter.andras@gmail.com>
2022-12-20 09:54:54 +0100

    Complete part 2 of day 4

--- src/day_4.py
+++ src/day_4.py
@@ -9,6 +9,14 @@ def is_one_section_subset_of_other(s1: tuple, s2: tuple) -> bool:
     return (intersect == s11) or (intersect == s22)
 
 
+def do_sections_overlap(s1: tuple, s2: tuple) -> bool:
+    s11 = set(range(s1[0], s1[1] + 1))
+    s22 = set(range(s2[0], s2[1] + 1))
+    intersect = s11.intersection(s22)
+
+    return len(intersect) != 0
+
+
 if __name__ == "__main__":
     elf_pairs = get_input_part_1()
 
@@ -19,3 +27,7 @@ if __name__ == "__main__":
     print(f"answer for part 1: {sum(subset_pairs)}")
 
 # --------
+
+    overlapping_pairs = [int(do_sections_overlap(*elf_pair)) for elf_pair in elf_pairs]
+
+    print(f"answer for part 2: {sum(overlapping_pairs)}")
