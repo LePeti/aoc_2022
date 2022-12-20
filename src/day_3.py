210810ac4711f81c72263db7edf8ffb731e2fd9e
Peter Lukacs <lukacs.peter.andras@gmail.com>
2022-12-20 07:44:52 +0100

    Complete part 2 of day 3

--- src/day_3.py
+++ src/day_3.py
@@ -1,4 +1,4 @@
-from input.day_3 import get_input
+from input.day_3 import get_input_part_1, get_input_part_2
 from string import ascii_lowercase
 
 
@@ -20,9 +20,13 @@ def get_common_element(rucksack) -> str:
     return common_element.pop()
 
 
+def get_common_element_from_group(r1: set, r2: set, r3: set) -> str:
+    return r1.intersection(r2).intersection(r3).pop()
+
+
 if __name__ == "__main__":
 
-    rucksacks = get_input()
+    rucksacks = get_input_part_1()
 
     all_common_elements = [get_common_element(rucksack) for rucksack in rucksacks]
 
@@ -33,3 +37,24 @@ if __name__ == "__main__":
     ]
 
     print(f"part 1 answer: {sum(priorities_of_common_elements)}")
+
+    # ---------
+
+    rucksacks = get_input_part_2()
+
+    rucksacks_grouped = [
+        [rucksacks[i], rucksacks[i + 1], rucksacks[i + 2]]
+        for i in range(0, len(rucksacks), 3)
+    ]
+
+    common_elements_in_groups = [
+        get_common_element_from_group(set(r1), set(r2), set(r3))
+        for r1, r2, r3 in rucksacks_grouped
+    ]
+
+    priorities_of_common_elements = [
+        item_priorities.get(common_element)
+        for common_element in common_elements_in_groups
+    ]
+
+    print(f"part 2 answer: {sum(priorities_of_common_elements)}")
