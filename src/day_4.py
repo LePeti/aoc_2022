dcdb7fe71bbd1dd9478694102b2e7527c5d46d5f
Peter Lukacs <lukacs.peter.andras@gmail.com>
2022-12-20 09:49:14 +0100

    Refactor: change functions name

--- src/day_4.py
+++ src/day_4.py
@@ -1,7 +1,7 @@
 from input.day_4 import get_input_part_1
 
 
-def do_sections_overlap(s1: tuple, s2: tuple) -> bool:
+def is_one_section_subset_of_other(s1: tuple, s2: tuple) -> bool:
     s11 = set(range(s1[0], s1[1] + 1))
     s22 = set(range(s2[0], s2[1] + 1))
     intersect = s11.intersection(s22)
@@ -12,8 +12,10 @@ def do_sections_overlap(s1: tuple, s2: tuple) -> bool:
 if __name__ == "__main__":
     elf_pairs = get_input_part_1()
 
-    overlapping_pairs = [int(do_sections_overlap(*elf_pair)) for elf_pair in elf_pairs]
+    subset_pairs = [
+        int(is_one_section_subset_of_other(*elf_pair)) for elf_pair in elf_pairs
+    ]
 
-    print(f"answer for part 1: {sum(overlapping_pairs)}")
+    print(f"answer for part 1: {sum(subset_pairs)}")
 
 # --------
