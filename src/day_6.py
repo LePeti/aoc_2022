d1c1d07e89ef8e820f3d77eb4382cb4690341668
Peter Lukacs <lukacs.peter.andras@gmail.com>
2022-12-21 14:52:14 +0100

    Refactor: open script for part 2

--- src/day_6.py
+++ src/day_6.py
@@ -1,12 +1,11 @@
 from input.day_6 import get_input
 
 
-def are_4_letters_unique(letters: str) -> bool:
-    assert len(letters) == 4, (
-        "input must be 4 letters, e.g. 'abcd'. "
-        f"Your input was '{letters}' (length of {len(letters)})"
+def are_n_letters_unique(letters: str, n: int) -> bool:
+    assert len(letters) == n, (
+        f"input must be {n} letters. Your input was '{letters}' (length of {len(letters)})"
     )
-    return len(set(letters)) == 4
+    return len(set(letters)) == n
 
 
 if __name__ == "__main__":
@@ -14,7 +13,9 @@ if __name__ == "__main__":
 
     i = 0
 
-    while not are_4_letters_unique(signal[i:i + 4]):
+    while not are_n_letters_unique(signal[i:i + 4], 4):
         i += 1
 
     print(f"Answer for part 1 is {i + 4}")
+
+# -----------
